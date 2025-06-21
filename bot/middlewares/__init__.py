bfc8218e3ca6bb5fc04d257a3cd5a96fbc27b022
abshka <markelofaleksei@gmail.com>
2025-06-21 19:22:08 +0300

    Remove bot functionality

--- bot/middlewares/__init__.py
+++
@@ -1,8 +0,0 @@
-# Middlewares for the Telegram bot
-# This file makes Python treat the directory `middlewares` as a package.
-
-# You can import specific middlewares here for easier access from other parts of the bot,
-# for example:
-# from .auth_middleware import AuthMiddleware
-
-# For now, it's empty as we haven't defined any specific middlewares yet.
--- bot/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# telegram-obsidian/bot/services/__init__.py
-# This file makes the 'services' directory a Python package.
-
-# You can optionally import modules here to make them available
-# when the package is imported, e.g.:
-# from .archiver import archive_data
